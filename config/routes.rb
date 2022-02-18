cf4d3d58eb66848ff0b42d23a5970060fdb7e74c
Romulo Rodrigues <biorrodrigues@gmail.com>
2022-02-17 23:42:42 -0300

    Working on initial steps on creating a CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+  root 'home#index' #action sao metodos que estao dnetro dos controlers
+  resources :users, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
